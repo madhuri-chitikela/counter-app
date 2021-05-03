e7dac4424ef429bced4b5c0ca1a9be4af7a386a6
Madhuri Chitikela <madhurisravan.chitikela@gmail.com>
2021-05-03 18:12:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+btns.forEach(function (btn) {
+    console.log(btn)
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        console.log(styles)
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        value.textContent = count;
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        else {
+            value.style.color = "#222";
+        }
+    })
+})
